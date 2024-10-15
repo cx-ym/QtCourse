c16a275610b981b33f560291db69c965cf5fc7fc
Ycx <1931315745@qq.com>
2024-10-15 21:15:39 +0800

    2024.10.15计算器大部分功能已经实现，剩下一些细节需要完善和bug需要修复

--- Calculator/mainwindow.cpp
+++ Calculator/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "qdebug.h"
+#include "math.h"
+#include "QKeyEvent"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,23 +10,57 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    digitBtns = {{Qt::Key_0, ui->btnNum0},
+        {Qt::Key_1, ui->btnNum1},
+        {Qt::Key_2, ui->btnNum2},
+        {Qt::Key_3, ui->btnNum3},
+        {Qt::Key_4, ui->btnNum4},
+        {Qt::Key_5, ui->btnNum5},
+        {Qt::Key_6, ui->btnNum6},
+        {Qt::Key_7, ui->btnNum7},
+        {Qt::Key_8, ui->btnNum8},
+        {Qt::Key_9, ui->btnNum9}
+    };
+    binaryBtns = {{Qt::Key_Plus, ui->btnPlus},
+        {Qt::Key_Minus, ui->btnMinus},
+        {Qt::Key_Slash, ui->btnDivide},
+        {Qt::Key_Asterisk, ui->btnMulti},
+        {Qt::Key_Enter, ui->btnEqual},
+    };
+
+
+
     //  将数字0-9的槽函数关联
-    connect(ui->btnNum0, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum1, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum2, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum3, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum4, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum5, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum6, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum7, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum8, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
-    connect(ui->btnNum9, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum0, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum1, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum2, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum3, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum4, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum5, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum6, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum7, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum8, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+    // connect(ui->btnNum9, SIGNAL(clicked(bool)), this, SLOT(btnNumClicked()));
+
+    foreach (auto btn, digitBtns) {
+        connect(btn, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    }
 
     // 将加减乘除符号的槽函数关联
-    connect(ui->btnDivide, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
-    connect(ui->btnPlus, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
-    connect(ui->btnMinus, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
-    connect(ui->btnMulti, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
+
+    foreach (auto btn, binaryBtns) {
+        connect(btn, SIGNAL(clicked()), this, SLOT(btnBinaryOperatorClicked()));
+    }
+    // connect(ui->btnDivide, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
+    // connect(ui->btnPlus, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
+    // connect(ui->btnMinus, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
+    // connect(ui->btnMulti, SIGNAL(clicked(bool)), this, SLOT(btnBinaryOperatorClicked()));
+
+    //另外几个
+    connect(ui->btnSqaure, SIGNAL(clicked(bool)), this, SLOT(btnUnaryOperatorClickead()));
+    connect(ui->btnSqrt, SIGNAL(clicked(bool)), this, SLOT(btnUnaryOperatorClickead()));
+    connect(ui->btnReciprocal, SIGNAL(clicked(bool)), this, SLOT(btnUnaryOperatorClickead()));
+    connect(ui->btnPercentage, SIGNAL(clicked(bool)), this, SLOT(btnUnaryOperatorClickead()));
 }
 
 MainWindow::~MainWindow()
@@ -35,6 +71,8 @@ MainWindow::~MainWindow()
 QString MainWindow::calculation(bool *ok)
 {
     double result = 0;
+    qDebug() << operands.size();
+    qDebug() << opcodes.size();
     if (operands.size() == 2 && opcodes.size() > 0) {
         //取操作数
         double operand1 = operands.front().toDouble();
@@ -43,20 +81,22 @@ QString MainWindow::calculation(bool *ok)
         operands.pop_front();
 
         //取操作符
-        QString op = opcodes.pop();
+        QString op = opcodes.front();
         opcodes.pop_front();
 
         if (op == "+") {
             result = operand1 + operand2;
         } else if (op == "-") {
             result = operand1 - operand2;
-        } else if (op == "*") {
+        } else if (op == "×") {
             result = operand1 * operand2;
         } else if (op == "/") {
             result = operand1 / operand2;
         }
-        // operands.push_back(QString::number(result));
-        ui->statusbar->showMessage("calculation is in progress");
+
+        operands.push_back(QString::number(result));
+
+        ui->statusbar->showMessage(QString("operands is %1, opcodes is %2").arg(operands.size()).arg(opcodes.size()));
     } else
         ui->statusbar->showMessage(QString("operands is %1, opcodes is %2").arg(operands.size()).arg(opcodes.size()));
 
@@ -76,6 +116,7 @@ void MainWindow::btnNumClicked()
         operand = "";
 
     operand += digit;
+
     ui->display->setText(operand);
 
 }
@@ -99,6 +140,8 @@ void MainWindow::on_btnDel_clicked()
 void MainWindow::on_btnClearAll_clicked()
 {
     operand.clear();
+    operands.clear();
+    opcodes.clear();
     ui->display->setText(operand);
 }
 
@@ -109,33 +152,75 @@ void MainWindow::btnBinaryOperatorClicked()
     if (operand != "") {
         operands.push_back(operand);
         operand = "";
-
-        opcodes.push_back(opcode);
     }
+    opcodes.push_back(opcode);
 
-    QString result = calculation();
-    // operands.push_back(result);
-    ui->display->setText(result);
+    if (operands.size() == 2) {
+        QString result = calculation();
+        ui->display->setText(result);
+    }
 }
 
 void MainWindow::btnUnaryOperatorClickead()
 {
     if (operand != "") {
-        operands.push_back(operand);
+        double result = operand.toDouble();
         operand = "";
+
+        QString op = qobject_cast<QPushButton *>(sender())->text();
+
+        if (op == "%")
+            result /= 100.0;
+        else if (op == "1/x")
+            result = 1 / result;
+        else if (op == "x^2")
+            result *= result;
+        else if (op == "√x")
+            result = sqrt(result);
+
+        ui->display->setText(QString::number(result));
     }
 }
 
 
 void MainWindow::on_btnEqual_clicked()
 {
-    // if (operand != "") {
-    //     operands.push_back(operand);
-    //     operand = "";
-    // }
+    if (operand != "") {
+        operands.push_back(operand);
+        operand = "";
+    }
 
     QString result = calculation();
-    // operands.push_back(result);
+    qDebug() << result;
     ui->display->setText(result);
+}
+
+
+void MainWindow::on_btnChange_clicked()
+{
+    double result = operands.front().toDouble() * -1;
+    operands.pop_front();
+    operands.push_back(QString::number(result));
+    qDebug() << "result=" << result;
+    qDebug() << operands.front();
+    ui->display->setText(QString::number(result));
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    foreach (auto btnkey, digitBtns.keys()) {
+        if (event->key() == btnkey)
+            digitBtns[btnkey]->animateClick();
+    }
+    foreach (auto btnkey, binaryBtns.keys()) {
+        if (event->key() == btnkey)
+            binaryBtns[btnkey]->animateClick();
+    }
+    if (event->key() == Qt::Key_Period) {
+        ui->btnSign->animateClick();
+    }
+    if (event->key() == Qt::Key_Backspace) {
+        ui->btnDel->animateClick();
+    }
 }
 
--- Calculator/mainwindow.h
+++ Calculator/mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 #include <QStack>
+#include <QMap>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,6 +24,8 @@ public:
     QString opcode;
     QStack<QString> operands;
     QStack<QString> opcodes;
+    QMap<int, QPushButton *> digitBtns;
+    QMap<int, QPushButton *> binaryBtns;
 
     QString calculation(bool *ok = NULL);
 
@@ -39,6 +43,10 @@ private slots:
     void on_btnClearAll_clicked();
 
     void on_btnEqual_clicked();
+
+    void on_btnChange_clicked();
+
+    virtual void keyPressEvent(QKeyEvent* event);
 
 private:
     Ui::MainWindow *ui;
